5cbadb149b9dc8c4a2680e3a7254543eed5fa572
shad-k <shadab.2005@gmail.com>
2017-04-03 20:23:05 +0530

    feat: Add cat level

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@ var ViewModel = function() {
 	this.name = ko.observable("Tabby");
 	this.imgSrc = ko.observable("img/9648464288_2516b35537_z.jpg");
 	this.imgAttribution = ko.observable("bla bla");
+	this.catLevel = ko.computed(function() {
+		if(this.clickCount() < 10) {
+			return "Newborn";
+		}
+		else {
+			return "Infant";
+		}
+	}, this);
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
