0a928b5f1b4100aa766d18515f05d9983697d0e5
shad-k <shadab.2005@gmail.com>
2017-04-04 19:33:06 +0530

    feat: Add multiple cats

--- js/app.js
+++ js/app.js
@@ -1,8 +1,45 @@
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable("Tabby");
-	this.imgSrc = ko.observable("img/9648464288_2516b35537_z.jpg");
-	this.imgAttribution = ko.observable("bla bla");
+var initialCats = [{
+			name: "tom",
+			imgSrc: "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg",
+			clickCount: 0,
+			imgAttribution: "bla bla",
+			nickNames: ["t-boy", "tabber", "table"]
+		},
+		{
+			name: "colt",
+			imgSrc: "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/140.jpg",
+			clickCount: 0,
+			imgAttribution: "bla bla",
+			nickNames: ["colty"]
+		},
+		{
+			name: "pete",
+			imgSrc: "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/287.jpg",
+			clickCount: 0,
+			imgAttribution: "bla bla",
+			nickNames: ["petey"]
+		},
+		{
+			name: "scott",
+			imgSrc: "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/286.jpg",
+			clickCount: 0,
+			imgAttribution: "bla bla",
+			nickNames: ["scotty"]
+		},
+		{
+			name: "smith",
+			imgSrc: "http://placekitten.com.s3.amazonaws.com/homepage-samples/200/138.jpg",
+			clickCount: 0,
+			imgAttribution: "bla bla",
+			nickNames: ["smoothie"]
+		}
+	];
+
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
 	this.catLevel = ko.computed(function() {
 		var title;
 		var clicks = this.clickCount();
@@ -22,19 +59,29 @@ var Cat = function() {
 		return title;
 	}, this);
 
-	this.nickNames = ko.observableArray(["t-boy", "tabs", "tabber", "table"]);
+	this.nickNames = ko.observableArray(data.nickNames);
 }
 
 var ViewModel = function() {
-	// Alt solution
-	// var self = this;
-	this.currentCat = ko.observable(new Cat());
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(cat) {
+		self.catList.push(new Cat(cat));
+	})
+
+	this.currentCat = ko.observable(this.catList()[0]);
 
 	this.incrementCounter = function() {
 		// Alt solution
 		// self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 
 		this.clickCount(this.clickCount() + 1);
+	}
+
+	this.changeCurrentCat = function(cat) {
+		self.currentCat(cat);
 	}
 };
 
